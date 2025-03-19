34ab3ac98a61c335a15354152fbeaa7ccc6616d9
David Gillespie <david.gillespie@digital.cabinet-office.gov.uk>
2025-03-19 08:49:30 +0000

    Fix the error when returning the default registry.

--- src/llmbo/registry.py
+++ src/llmbo/registry.py
@@ -70,9 +70,9 @@ class ModelAdapterRegistry:
         for pattern, adapter in cls._adapters:
             if pattern.search(model_name):
                 return adapter
-            else:
-                cls.logger.warning(
-                    f"No pattern found for {model_name}, returning default ModelAdapter. ",
-                    "This model is unsupported it may not work as expected.",
-                )
-                return DefaultAdapter
+
+        cls.logger.warning(
+            f"No pattern found for {model_name}, returning default ModelAdapter. ",
+            "This model is unsupported it may not work as expected.",
+        )
+        return DefaultAdapter
